eef23c9944fe46fb665802af098df38b2c2cb568
Lexa Lechuga <raallero@gmail.com>
2023-07-15 20:12:40 -0600

    Segunda version funcional. +agregado letras ya adivinadas +mejora de la interfaz de texto para una mejor experiencia de juego
    Divertiros con el juego!

--- proyecto_ada.py
+++ proyecto_ada.py
@@ -41,10 +41,12 @@ def juegoTerminado(resultado):
   return not any(filter(lambda x: (x == '-'), resultado))
 
 
-print('Juego nuevo! \n')
+print('\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nJuego nuevo! \n')
 
 for k in range(len(pal)):
   res.append('-')
+
+letCorr = []
 
 while vid >= 1 and juegoTerminado(res) == False:
 
@@ -63,17 +65,18 @@ while vid >= 1 and juegoTerminado(res) == False:
   compara(let, pal, res)
   vid -= buenoMalo(let, pal)
 
-  if buenoMalo(let, pal) == 0:
-    print(f'\nAcertado! Va por buen camino \n')
+  if (let in letCorr):
+    print(f'\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nYa has adivinado esa letra, esoge otra\n')
+
+  elif buenoMalo(let, pal) == 0:
+    letCorr.append(let)
+    print(f'\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nAcertado! Va por buen camino\n')
+  
   else:
-    print(f'\nIncorrecto, usted pierde una vida \n')
+    print(f'\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nIncorrecto, usted pierde una vida\n')
   print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 
 if juegoTerminado(res) == True:
-  print(f'Fin del juego\n\nFelicidades, ha conseguido adivinar la palabra')
+  print(f'\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nFin del juego\n\nFelicidades, ha conseguido adivinar la palabra\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
 elif vid == 0:
-  print(f'Fin del juego\n\nNo ha conseguido adivinar la palabra \n\nA que la proxima lo consigue!')
-
-
-
-
+  print(f'\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\nFin del juego\n\nNo ha conseguido adivinar la palabra \n\nA que la proxima lo consigue!\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n')
